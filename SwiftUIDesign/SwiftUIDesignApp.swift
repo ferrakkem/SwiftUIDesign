d096011094e1042aa8eb40c3f261376e2e57e306
Md.Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2023-05-06 22:43:44 -0300

    Initial Commit

---
+++ SwiftUIDesign/SwiftUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignApp.swift
+//  SwiftUIDesign
+//
+//  Created by Ferrakkem Bhuiyan on 2023-05-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
